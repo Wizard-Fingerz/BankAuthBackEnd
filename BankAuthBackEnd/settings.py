f36dc0ac35866fd3097a3a141f0d31537368aadb
Wizard_Fingerz <adewale.oladiti@tech-u.edu.ng>
2023-05-26 01:14:41 +0000

    debug updated

--- BankAuthBackEnd/settings.py
+++ BankAuthBackEnd/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-!jaucuju141=s4i@3zolbmie9x3(uc+(w&ej7mv=mu9hjg$d%k"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['localhost', '127.0.0.1', '192.168.79.171', 'BankAuth.pythonanywhere.com']
 
