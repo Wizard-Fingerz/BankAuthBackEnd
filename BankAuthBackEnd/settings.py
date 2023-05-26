0e4d61a48a29b26a6ca60d81dc0a72bc943f185a
Wizard_Fingerz <adewale.oladiti@tech-u.edu.ng>
2023-05-26 01:13:53 +0000

    requirement.txt updated

--- BankAuthBackEnd/settings.py
+++ BankAuthBackEnd/settings.py
@@ -26,11 +26,8 @@ SECRET_KEY = "django-insecure-!jaucuju141=s4i@3zolbmie9x3(uc+(w&ej7mv=mu9hjg$d%k
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-<<<<<<< HEAD
 ALLOWED_HOSTS = ['localhost', '127.0.0.1', '192.168.79.171', 'BankAuth.pythonanywhere.com']
-=======
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', '192.168.79.171', 'BankAuth.pythonanywhere.com',]
->>>>>>> 01df2bf51d76744d4aab8eacc008363006e9ffed
+
 
 CORS_ORIGIN_ALLOW_ALL = True
 
@@ -70,6 +67,7 @@ MIDDLEWARE = [
     "django.middleware.common.CommonMiddleware",
     'corsheaders.middleware.CorsMiddleware',
     "django.middleware.security.SecurityMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
@@ -99,9 +97,9 @@ WSGI_APPLICATION = "BankAuthBackEnd.wsgi.application"
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static')
+    os.path.join(BASE_DIR, 'staticfiles')
    ]
-STATIC_URL = '/static/'
+STATIC_URL = '/staticfiles/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
@@ -162,7 +160,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = "static/"
+STATIC_URL = "staticfiles/"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
